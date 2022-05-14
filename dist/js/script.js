491369a89b0ec45d5a931984b2ea9ab7da6afda8
seutiadji <loyaldji@gmail.com>
2022-05-14 16:06:47 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,20 @@
+//navbarfix
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav){
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed')
+    }
+};
+
+//humberger
+const humberger = document.querySelector('#humberger');
+const navMenu = document.querySelector('#nav-menu');
+
+humberger.addEventListener('click', function () {
+  humberger.classList.toggle('humberger-active');  
+navMenu.classList.toggle('hidden');
+});
